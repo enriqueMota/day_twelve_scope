5a7fd677ebe113ebb00e84c5ccb5e52409c8d2d3
Enrique mota <enrique.motadejesus@gmail.com>
2023-11-26 12:44:43 -0400

    Constants in python

---
+++ constant.py
@@ -0,0 +1,8 @@
+
+
+#? Global Constants
+#? The difference between variables and constants is the casing,
+#? Constants are in UPPERCASE as shown below.
+PI = 3.14159
+URL = "www.google.com"
+pi = 3.14159
